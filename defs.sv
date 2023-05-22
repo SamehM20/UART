0ac5b25e4dc8b1f8441043ae2b415ad915be72e6
SamehM20 <134065615+SamehM20@users.noreply.github.com>
2023-05-22 22:46:32 +0300

    Update defs.sv

--- defs.sv
+++ defs.sv
@@ -1,11 +1,16 @@
 package defs;
     // For the Baud Generator.
     parameter Clock_freq = 50_000_000;
-    parameter baud_tick_1200 = Clock_freq / (1200*16);
-    parameter baud_tick_2400 = baud_tick_1200 / 2;
-    parameter baud_tick_4800 = baud_tick_2400 / 2;
-    parameter baud_tick_9600 = baud_tick_4800 / 2;
-    parameter baud_calc = $clog2(baud_tick_1200/2 + 1);    // The number of bits required to represent the next signals.
+    // Note 'baud_1' must be the slowest baud rate or change the parameter 'baud_tick_1' in 'baud_calc' to whatever the slowest.
+    parameter baud_1 = 1200;
+    parameter baud_2 = 2400;
+    parameter baud_3 = 4800;
+    parameter baud_4 = 9600;
+    parameter baud_tick_1 = Clock_freq / (baud_1*16);
+    parameter baud_tick_2 = Clock_freq / (baud_2*16);
+    parameter baud_tick_3 = Clock_freq / (baud_3*16);
+    parameter baud_tick_4 = Clock_freq / (baud_4*16);
+    parameter baud_calc = $clog2(baud_tick_1/2 + 1);    // The number of bits required to represent the next signals.
 
     // For the UART Receiver.
     parameter SAMPLE_CONST = 16;
@@ -14,11 +19,11 @@ package defs;
     parameter count_bits = $clog2(N);
 
     function void notify_baud();
-        $display("Working with a Clock freqency of: %d, and The baud tick is every: %d clock cycles.", Clock_freq, baud_tick_1200);
-        $display("Baud_Tick value for 1200: %d", baud_tick_1200);
-        $display("Baud_Tick value for 2400: %d", baud_tick_2400);
-        $display("Baud_Tick value for 4800: %d", baud_tick_4800);
-        $display("Baud_Tick value for 9600: %d", baud_tick_9600);
+        $display("Working with a Clock freqency of: %d, and The slowest baud tick is every: %d clock cycles.", Clock_freq, baud_tick_1);
+        $display("Baud_Tick value for %d: %d", baud_1, baud_tick_1);
+        $display("Baud_Tick value for %d: %d", baud_2, baud_tick_2);
+        $display("Baud_Tick value for %d: %d", baud_3, baud_tick_3);
+        $display("Baud_Tick value for %d: %d", baud_4, baud_tick_4);
         $display("The required number of bits to represent the tick rate is: %d", baud_calc);
     endfunction
 
